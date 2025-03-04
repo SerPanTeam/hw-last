05b7bb2315d8a076372e01256167b69da9ee9725
Panchenko Serhii <panchenko.serhii@hotmail.com>
2025-03-04 10:40:01 +0100

    Remove files from index to reset line endings

--- src/magazine/dto/update-magazine.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMagazineDto } from './create-magazine.dto';
-
-export class UpdateMagazineDto extends PartialType(CreateMagazineDto) {}
